d04c17b56a7c8ea64de29c9e60eecd70bfc503b3
gl.alaimo <gl.alaimo@protonmail.com>
2026-01-11 17:27:33 +0100

    Localizzazione scripts

---
+++ carica_tempo.py
@@ -0,0 +1,27 @@
+"""Caricamento manuale del tempo di risoluzione del cubo al database."""
+from datetime import datetime
+
+from rubik_library import controllo_nuovo_record
+
+
+print("Inserisci i seguenti dati:")
+data_risoluzione = input("Data (ANNO-MESE-GIORNO) oppure 'oggi': ")
+if data_risoluzione == "oggi":
+    data_risoluzione = datetime.now().strftime("%Y-%m-%d")
+
+cubo = input("Tipo di cubo: ")
+aggiungere_altra_soluzione = "si"
+
+while aggiungere_altra_soluzione == "si":
+    minuti = int(input("Minuti: "))
+    secondi = int(input("Secondi: "))
+    millesimi = float(input("Millesimi di secondo (per esempio 0.43): "))
+    tempo_impiegato = (minuti * 60) + secondi + millesimi
+
+    controllo_nuovo_record(tempo_impiegato, cubo)
+
+    with open(file="database.csv", mode="a") as db_file:
+        db_file.write(f"{data_risoluzione}\t{tempo_impiegato}\t{cubo}\n")
+
+    print("Tempo risoluzione aggiunto al database:", data_risoluzione, tempo_impiegato, cubo)
+    aggiungere_altra_soluzione = input("\nVuoi inserire un altro tempo per lo stesso cubo? (si o no): ")
--- rubik_library.py
+++ rubik_library.py
@@ -1,51 +1,49 @@
-""" Rubik timer library"""
+"""Libreria Rubik timer"""
 
 from datetime import datetime
-import pandas as pd
-import random
-import time
-from pyfiglet import Figlet
+from pandas import read_csv
+from time import time
 
 
-def convert_seconds(seconds:int):
-    """Conversione dei secondi in minuti e secondi"""
-    minutes, seconds = divmod(seconds, 60)
-    minutes = int(minutes)
-    seconds = round(seconds, 2)
-    return minutes, seconds
+def conversione_secondi(secondi:int):
+    """Converte i secondi in minuti e secondi"""
+    minuti, secondi = divmod(secondi, 60)
+    minuti = int(minuti)
+    secondi = round(secondi, 2)
+    return minuti, secondi
 
 
-def get_record(cubo:str):
-    """Get current record from dataframe"""
-    df = pd.read_csv("database.csv", sep="\t")
-    record_solves = df[df["Cubo"] == cubo].min()
-    record_time = record_solves["Secondi"]
-    return record_time
+def ricerca_record(cubo:str):
+    """Ricerca il record attuale nel database"""
+    df = read_csv("database.csv", sep="\t")
+    record_risoluzione = df[df["Cubo"] == cubo].min()
+    tempo_record = record_risoluzione["Secondi"]
+    return tempo_record
 
 
-def is_new_record(tempo_impiegato:int, cubo:str):
-    """Check if record has been beaten"""
-    my_record = get_record(cubo)
-    if tempo_impiegato < my_record:
-        record_min, record_sec = convert_seconds(my_record)
-        current_min, current_sec = convert_seconds(tempo_impiegato)
-        print(f"Hai battuto il tuo record di {record_min-current_min} minuti e {abs((record_sec-current_sec).__round__(2))} secondi!!!\n"
+def controllo_nuovo_record(tempo_impiegato:int, cubo:str):
+    """Controlla se il record è stato battuto."""
+    mio_record = ricerca_record(cubo)
+    if tempo_impiegato < mio_record:
+        record_min, record_sec = conversione_secondi(mio_record)
+        minuti_attuali, secondi_attuali = conversione_secondi(tempo_impiegato)
+        print(f"Hai battuto il tuo record di {record_min-minuti_attuali} minuti e {abs((record_sec-secondi_attuali).__round__(2))} secondi!!!\n"
               f"Record precedente {record_min} minuti e {record_sec} secondi")
 
 
 def calcolo_tempo(cubo:str):
-    """Calculate solving time"""
+    """Calcola il tempo impiegato."""
     input("Premi invio per iniziare ")
     finito = False
     tempo_parziale = 0
 
     while finito == False:
-        ora_iniziale = time.time()
+        ora_iniziale = time()
         stop = input("Premi 'p' ed invio se vuoi mettere in pausa o invio per terminare: ")
         if stop == "p":
-            ora_finale = time.time()
+            ora_finale = time()
             tempo_parziale = (tempo_parziale + ora_finale - ora_iniziale).__round__(2)
-            min, sec = convert_seconds(tempo_parziale)
+            min, sec = conversione_secondi(tempo_parziale)
             if min == 1:
                 print(f"In pausa... Tempo impiegato fino ad ora: {min} minuto e {sec} secondi")
             else:
@@ -54,10 +52,10 @@ def calcolo_tempo(cubo:str):
         else:
             finito = True
 
-    ora_finale = time.time()
+    ora_finale = time()
     tempo_impiegato = (ora_finale - ora_iniziale).__round__(2) + tempo_parziale
     print("Fine!\n")
-    min, sec = convert_seconds(tempo_impiegato)
+    min, sec = conversione_secondi(tempo_impiegato)
     if min == 1:
         print(f"Tempo impiegato: {min} minuto e {sec} secondi")
     else:
@@ -66,24 +64,23 @@ def calcolo_tempo(cubo:str):
 
 
 def salvataggio_dati(tempo, cubo):
-    """Save time into the database"""
+    """Salva il tempo di risoluzione nel database"""
     data = datetime.now().date()
     with open(file="database.csv", mode="a") as db_file:
         db_file.write(f"{data}\t{tempo}\t{cubo}\n")
 
 
 def diffs_media_tempo_attuale(cubo:str, tempo_impiegato:int):
-    """Calcolo diff tempo soluzione media e tempo soluzione attuale"""
-    df = pd.read_csv("database.csv", sep="\t")
+    """Calcola la diffeerenza del tempo medio di risoluzione e il tempo di risoluzione attuale"""
+    df = read_csv("database.csv", sep="\t")
     media_df = df[df["Cubo"] == cubo]
     media = media_df["Secondi"].mean()
     media = media.__round__(2)
-    # conversione media da secondi in minuti e secondi
-    media_in_minuti = convert_seconds(media)
-    formatted_media_in_minuti = (media_in_minuti[0], str(media_in_minuti[1]))
-    formatted_media_in_minuti = f"{media_in_minuti[0]} minuti e {media_in_minuti[1]} secondi"
+    media_in_minuti = conversione_secondi(media)
+    media_in_minuti_formattata = (media_in_minuti[0], str(media_in_minuti[1]))
+    media_in_minuti_formattata = f"{media_in_minuti[0]} minuti e {media_in_minuti[1]} secondi"
 
-    print("La tua media attuale è di", media, "secondi, ovvero", formatted_media_in_minuti)
+    print("La tua media attuale è di", media, "secondi, ovvero", media_in_minuti_formattata)
     if tempo_impiegato < media:
         print(f"Ci hai impiegato {(media-tempo_impiegato).__round__(2)} secondi in meno del solito")
     elif tempo_impiegato > media:
@@ -92,12 +89,12 @@ def diffs_media_tempo_attuale(cubo:str, tempo_impiegato:int):
         print("Ci hai impiegato come al solito")
 
 
-def diffs_record_tempo_attuale(record:int, tempo_impiegato:int):
-    """Calcolo diff tempo record personale e tempo soluzione attuale"""
+def diff_record_tempo_attuale(record:int, tempo_impiegato:int):
+    """Calcola la differenza di tempo tra il record personale e il tempo di risoluzione attuale"""
     if tempo_impiegato > record:
         print(f"Ci hai impiegato", (tempo_impiegato-record).__round__(2), "secondi in più rispetto al record")
     elif tempo_impiegato == record:
         print("Hai eguaglito il tuo record")
     else:
-        pass # Informazioni sul record in questo caso verrano date dalla funzione is_new_record()
+        pass # Informazioni sul record in questo caso verrano date dalla funzione controllo_nuovo_record()
 
