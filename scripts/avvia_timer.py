2b75ca0b6d4ffb52d47d935f0db3f92a565015eb
gl.alaimo <gl.alaimo@protonmail.com>
2026-02-19 19:02:06 +0100

    Aggiunta immagini e inserimento cubi in minuscolo

--- scripts/avvia_timer.py
+++ scripts/avvia_timer.py
@@ -13,13 +13,13 @@ def funzione_principale():
     moviementi_cubi_piccoli = ['U', 'D', 'R', 'L', 'F', 'B', 'U1°', 'D1°', 'R1°', 'L1°', 'F1°', 'B1°', 'U2', 'D2', 'R2', 'L2', 'F2', 'B2']
     moviementi_cubi_grandi = ["u", "d", "r", "l", "f", "b", "u1°", "d1°", "r1°", "l1°", "f1°", "b1°", "u2", "d2", "r2", "l2", "f2", "b2", "Uu2", "Dd2", "Rr2", "Ll2", "Ff2", "Bb2", "U2", "D2", "R2", "L2", "F2", "B2", "U1°", "D1°", "R1°", "L1°", "F1°", "B1°"]
 
-    cubo = input("Digita il tipo di cubo (ad esempio 2x2, 3x3, Megaminx): ")
-    if cubo == "4x4" or cubo == "5x5":
-        mescolamento_random_4x4_5x5 = [choice(moviementi_cubi_grandi) for i in range(20)]
-        print("Movimenti casuali suggeriti per mischiare il cubo:\n",mescolamento_random_4x4_5x5)
+    cubo = input("Digita il tipo di cubo (ad esempio 2x2x2, 3x3x3, Megaminx): ").lower()
+    if cubo == "4x4x4" or cubo == "5x5x5":
+        mescolamento_random_4x4x4_5x5x5 = [choice(moviementi_cubi_grandi) for i in range(20)]
+        print("Movimenti casuali suggeriti per mischiare il cubo:\n",mescolamento_random_4x4x4_5x5x5)
     else:
-        mescolamento_random_2x2_3x3 = [choice(moviementi_cubi_piccoli) for i in range(20)]
-        print("Movimenti casuali suggeriti per mischiare il cubo:\n",mescolamento_random_2x2_3x3)
+        mescolamento_random_2x2x2_3x3x3 = [choice(moviementi_cubi_piccoli) for i in range(20)]
+        print("Movimenti casuali suggeriti per mischiare il cubo:\n",mescolamento_random_2x2x2_3x3x3)
 
     record_personale = modulo_rubik.ricerca_record(cubo)
     if record_personale != 0:
