eb07ed6616f5f7d54dbdad4adff5048fd37ac855
gl.alaimo <gl.alaimo@protonmail.com>
2026-06-01 11:48:01 +0200

    Aggiornamento da main

--- scripts/simula_competizione.py
+++ scripts/simula_competizione.py
@@ -1,8 +1,7 @@
-"""Script che calcola il tempo di 5 risoluzioni e fornisce la media dei risultati escludendo
-quelli con tempo minimo e massimo"""
+"""Script che calcola il tempo di 5 risoluzioni (oppure 3 nel caso di cubi grandi) e fornisce la media dei risultati escludendo
+quelli con tempo minimo e massimo."""
 
 from time import time
-from pyfiglet import Figlet
 import modulo_rubik
 
 
@@ -17,8 +16,7 @@ lista_risultati = []
 
 def funzione_principale():
     """Funzione principale"""
-    titolo = Figlet(font="speed") # http://www.figlet.org/examples.html
-    print(titolo.renderText("Timer cubo di Rubik"))
+    modulo_rubik.mostra_titolo()
     cubo = input("Digita il tipo di cubo (ad esempio 2x2x2, 3x3x3, megaminx): ").lower()
     record_personale = modulo_rubik.mostra_record(cubo=cubo)
     media = modulo_rubik.calcolo_media_assoluta(cubo=cubo)
@@ -42,6 +40,7 @@ def funzione_principale():
             modulo_rubik.genera_movimenti_casuali(lista_movimenti=movimenti_cubi_grandi)
 
         input("Premi invio per iniziare: ")
+        print("Timer avviato!")
         ora_iniziale = time()
         input("Premi invio per terminare: ")
         ora_finale = time()
